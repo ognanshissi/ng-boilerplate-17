5e7c4c8f75809ca1a809209d4dbec4b1a1686bed
Ambroise BAZIE <ambroise.bazie@infisoftware.net>
2024-03-29 16:37:09 +0000

    Update styles

--- src/app/shared/ui/datepicker/datepicker.component.ts
+++ src/app/shared/ui/datepicker/datepicker.component.ts
@@ -248,7 +248,8 @@ export class CoreDatepicker
   }
 
   public keyDownNumberCharType(event: KeyboardEvent) {
-    if (!['0', '1', '2', '3', '4', '5', '6', '7', '8', 9].includes(event.key)) {
+
+    if (!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(event.key)) {
       event.preventDefault();
     }
   }
--- src/app/shared/ui/form-field/form-label.component.ts
+++ src/app/shared/ui/form-field/form-label.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core'
+import {Component} from '@angular/core'
 
 @Component({
-    selector: '[coreLabel]',
-    template: `<ng-content></ng-content>`,
-    standalone: true,
-    styles: [
-        `
-            [coreLabel] {
-                @apply block w-full;
-            }
-        `,
-    ],
+  selector: '[coreLabel]',
+  template: `
+    <ng-content></ng-content>`,
+  standalone: true,
+  styles: [
+    `
+      [coreLabel] {
+        @apply block w-full text-sm;
+      }
+    `,
+  ],
 })
-export class CoreLabel {}
+export class CoreLabel {
+}
